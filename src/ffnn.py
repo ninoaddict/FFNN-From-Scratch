fix: change rms normalization
Previously, the rms normalization was done on the layer weights instead of linear transformation of the input vector Wx.

src/ffnn.py
@@ -19,6 +19,7 @@ class FFNN:
         mean=0,
         variance=1,
         seed=None,
+        use_rmsnorm=False
     ):
         self.layers = []
         self.layer_sizes = layer_sizes
@@ -36,6 +37,7 @@ class FFNN:
                     mean,
                     variance,
                     seed,
+                    use_rmsnorm
                 )
             )
 
@@ -56,18 +58,19 @@ class FFNN:
                 reg_l1 = reg_lambda * np.sign(layer.weights)
                 layer.weights -= learning_rate * (layer.grad_weights + reg_l1)
                 layer.biases -= learning_rate * layer.grad_biases
+                if layer.use_rmsnorm:
+                    layer.rmsnorm.g -= learning_rate * layer.rmsnorm.grad_g
             elif regularization == "l2":
                 reg_l2 = reg_lambda * 2 * layer.weights
                 layer.weights -= learning_rate * (layer.grad_weights + reg_l2)
                 layer.biases -= learning_rate * layer.grad_biases
+                if layer.use_rmsnorm:
+                    layer.rmsnorm.g -= learning_rate * layer.rmsnorm.grad_g
             else:
                 layer.weights -= learning_rate * layer.grad_weights
                 layer.biases -= learning_rate * layer.grad_biases
-
-    def __rms_norm(self):
-        for layer in self.layers:
-            rms = np.sqrt(np.mean(layer.weights**2) + 1e-6)
-            layer.weights = layer.weights / rms
+                if layer.use_rmsnorm:
+                    layer.rmsnorm.g -= learning_rate * layer.rmsnorm.grad_g
 
     def train(
         self,
@@ -80,7 +83,6 @@ class FFNN:
         epochs=100,
         batch_size=32,
         verbose=1,
-        rms_norm=False,
         regularization=None,
         reg_lambda=0.0,
     ):
@@ -133,8 +135,6 @@ class FFNN:
 
                 self.__backward(loss_grad)
                 self.__update_weights(learning_rate)
-                if rms_norm:
-                    self.__rms_norm()
                 epoch_loss += loss
                 if verbose:
                     pbar.update(1)

src/layer.py
@@ -1,5 +1,6 @@
 import numpy as np
 from actfunc import ActivationFunctions
+from rmsnorm import RMSNorm
 
 
 class Layer:
@@ -14,6 +15,7 @@ class Layer:
         mean: float,
         variance: float,
         seed,
+        use_rmsnorm: bool=True
     ):
         self.activation_name = activation
         self.activation = getattr(ActivationFunctions, activation)
@@ -48,10 +50,20 @@ class Layer:
         self.output = None
         self.grad_weights = np.zeros((input_size, output_size))
         self.grad_biases = np.zeros((1, output_size))
+        
+        self.use_rmsnorm = use_rmsnorm
+        if self.use_rmsnorm:
+            self.rmsnorm = RMSNorm(output_size)
+        else:
+            self.rmsnorm = None
 
     def forward(self, x):
         self.input = x
-        self.output = self.activation(x @ self.weights + self.biases)
+        z = x @ self.weights
+        if self.use_rmsnorm:
+            z = self.rmsnorm.forward(z)
+        z = z + self.biases
+        self.output = self.activation(z)
         return self.output
 
     def backward(self, grad_output):
@@ -66,7 +78,13 @@ class Layer:
         else:
             activation_grad = self.activation(self.output, derivative=True)
             grad = grad_output * activation_grad
-
-        self.grad_weights = self.input.T @ grad
+        
+        # calculate the bias grad
         self.grad_biases = np.sum(grad, axis=0, keepdims=True)
+
+        # calculate the weight grad and g grad if applicable
+        if self.use_rmsnorm:
+            grad = self.rmsnorm.backward(grad)        
+        
+        self.grad_weights = self.input.T @ grad
         return grad @ self.weights.T